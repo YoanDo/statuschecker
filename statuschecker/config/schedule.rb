969516520b4b0f4702958df83d97a394c5991791
Yoan Dorny <y.n.dorny@gmail.com>
2017-09-24 20:16:35 +0200

    gem fight

--- statuschecker/config/schedule.rb
+++ statuschecker/config/schedule.rb
@@ -1,4 +1,5 @@
-set :environment, "development"
+env :PATH, ENV['PATH']
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 every 2.minutes do
-  rake "check:all"
+  rake 'check:all'
 end
